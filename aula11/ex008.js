2ffd1f775d0efcd7c830209edeb6f124e0ec4cd1
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-10 10:32:48 -0300

    aula11 feita

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 30
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você foi multado pois estava acima de 60 km/h estava á ${vel}km/h`)
+}   
+console.log(`Dirija sempre usando cinto de segurança!`)
