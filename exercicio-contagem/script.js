7f3b14d0fe9acd5fe4ff8b29d94b3352aa2550ae
Rafael Nestor <rafaelnestor01@gmail.com>
2025-10-13 12:55:54 -0300

    modificações

---
+++ exercicio-contagem/script.js
@@ -0,0 +1,14 @@
+function contar(){
+    let inicio = Number(document.getElementById('inicio').value)
+    let fim = Number(document.getElementById('fim').value)
+    let passo = Number(document.getElementById('passo').value)
+    let res = document.getElementById('res')
+    res.innerHTML = `${inicio} >`
+    for(let i = (inicio + passo); i <= fim; i = i + passo){
+        res.innerHTML += `   ${i}   >`
+    }
+    res.innerHTML += '🏁'
+    
+    
+    //res.innerHTML = `${inicio} ${fim} ${passo}`
+}
